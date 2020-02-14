9fd6dd355b6ee9244ecb2e35bd440678294fa0c7
Tim Schlueter <2689079+modelrockettier@users.noreply.github.com>
2020-02-13 22:48:48 -0800

    Fixed travis ci builds + arm64 + deb builds
    Also fixed building adcrush with SYSTEMD_SUPPORT and changed the default log
    file from /var/log/uhub/uhub.log to just /var/log/uhub.log in several places.

--- autotest/travis/build-and-test.sh
+++ autotest/travis/build-and-test.sh
@@ -1,43 +1,68 @@
-#!/bin/sh
+#!/bin/bash
 
 set -x
 set -e
 
-export CFLAGS="$(dpkg-buildflags --get CFLAGS) $(dpkg-buildflags --get CPPFLAGS)"
-export LDFLAGS="$(dpkg-buildflags --get LDFLAGS) -Wl,--as-needed"
+# Test creating the docker image
+if [ "${CONFIG}" = "docker" ]; then
+	docker build -t uhub:$(uname -m) .
 
-mkdir -p builddir
-cd builddir
+# Test creating the debian package
+elif [ "${CONFIG}" = "deb" ]; then
+	dpkg-buildpackage -us -uc -b -jauto
 
-CMAKEOPTS="..
-           -DCMAKE_INSTALL_PREFIX=/usr"
+	du -shc ../uhub*.deb
 
-if [ "${CONFIG}" = "full" ]; then
-    CMAKEOPTS="${CMAKEOPTS}
-               -DCMAKE_BUILD_TYPE=Debug
-               -DLOWLEVEL_DEBUG=ON
-               -DSSL_SUPPORT=ON
-               -DUSE_OPENSSL=ON
-               -DADC_STRESS=ON"
+	sudo dpkg --install ../uhub*.deb
+
+	PLUGIN_DIR=$(perl -ne 'm@^#?plugin (/usr/lib.*/uhub)/mod_.*\.so@ and print $1,"\n" and exit 0' /etc/uhub/plugins.conf)
+	if [ -z "${PLUGIN_DIR}" ]; then
+		echo "Plugin dir not found" >&2
+		exit 2
+	fi
+
+	du -shc /etc/uhub/ /usr/bin/uhub* "${PLUGIN_DIR}"
+
+# Test the vanilla cmake build+install
+elif [ "${CONFIG}" = "full" ] || [ "${CONFIG}" = "minimal" ]; then
+	rm -rf builddir
+	mkdir builddir
+	cd builddir
+
+	CMAKEOPTS="..
+			   -DCMAKE_INSTALL_PREFIX=/usr
+			   -DPLUGIN_DIR=/usr/lib/uhub"
+
+	if [ "${CONFIG}" = "full" ]; then
+		CMAKEOPTS="${CMAKEOPTS}
+				   -DCMAKE_BUILD_TYPE=Debug
+				   -DLOWLEVEL_DEBUG=ON
+				   -DSSL_SUPPORT=ON
+				   -DUSE_OPENSSL=ON
+				   -DADC_STRESS=ON"
+		if [ "$TRAVIS_OS_NAME" = "linux" ]; then
+			CMAKEOPTS="${CMAKEOPTS}
+					   -DSYSTEMD_SUPPORT=ON"
+		fi
+	else
+		CMAKEOPTS="${CMAKEOPTS}
+				   -DCMAKE_BUILD_TYPE=Release
+				   -DLOWLEVEL_DEBUG=OFF
+				   -DSSL_SUPPORT=OFF
+				   -DADC_STRESS=OFF"
+	fi
+
+	cmake ${CMAKEOPTS}
+	make VERBOSE=1 -j3
+
+	make VERBOSE=1 -j3 autotest-bin
+	./autotest-bin
+
+	sudo make install
+	du -shc /etc/uhub/ /usr/bin/uhub* /usr/lib/uhub/
+
 else
-    CMAKEOPTS="${CMAKEOPTS}
-               -DCMAKE_BUILD_TYPE=Release
-               -DLOWLEVEL_DEBUG=OFF
-               -DSSL_SUPPORT=OFF
-               -DADC_STRESS=OFF"
+	echo "Unknown config: ${CONFIG}" >&2
+	exit 5
 fi
-
-
-cmake ${CMAKEOPTS} \
-      -DCMAKE_C_FLAGS="${CFLAGS}" \
-      -DCMAKE_EXE_LINKER_FLAGS="${LDFLAGS}"
-make VERBOSE=1
-
-
-make VERBOSE=1 autotest-bin
-./autotest-bin
-
-
-sudo make install
-du -shc /etc/uhub/ /usr/bin/uhub* /usr/lib/uhub/
 
--- autotest/travis/install-build-depends.sh
+++ autotest/travis/install-build-depends.sh
@@ -1,10 +1,30 @@
-#!/bin/sh
+#!/bin/bash
 
-sudo apt-get update -qq
+set -x
+set -e
 
-sudo apt-get install -qq cmake
+case "${CONFIG}" in
+	deb|docker|full|minimal) ;;
+	*)
+		echo "Unknown config: ${CONFIG}" >&2
+		exit 5 ;;
+esac
 
-if [ "${CONFIG}" = "full" ]; then
-    sudo apt-get install -qq libsqlite3-dev libssl-dev
+if [ "$TRAVIS_OS_NAME" = "linux" ]; then
+	case "${CONFIG}" in
+		full) EXTRA="git libssl-dev libsystemd-dev pkg-config" ;;
+		deb)  EXTRA="debhelper fakeroot git libssl-dev pkg-config" ;; # libsystemd-dev
+		docker) exit 0 ;;
+	esac
+
+	sudo apt-get update -q
+	sudo apt-get install -y cmake make libsqlite3-dev $EXTRA
+
+#elif [ "$TRAVIS_OS_NAME" = "osx" ]; then
+#	brew update
+#	brew install cmake openssl pkg-config sqlite
+
+else
+	echo "Unknown OS: ${TRAVIS_OS_NAME}" >&2
+	exit 6
 fi
-
